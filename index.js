b2395e10964bfa69dd47ae7aaf57b573dc7b0f95
ftwdsyui <gs1gemio987@hotmail.com>
2024-05-12 03:02:12 +0700

    Initial commit

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-function removeDuplicates(arr) {
-  return [...new Set(arr)];
+function isValidBST(root) {
+  return isValid(root, null, null);
+}
+function isValid(node, min, max) {
+  if (!node) return true;
+  if ((min !== null && node.val <= min) || (max !== null && node.val >= max))
+    return false;
+  return (
+    isValid(node.left, min, node.val) && isValid(node.right, node.val, max)
+  );
 }
